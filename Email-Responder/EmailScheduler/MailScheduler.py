a2dd4e8c520df1eab98aed031e831ab7f4dde00e
Shreyash Padeer <108185903+Shreyash-Padeer@users.noreply.github.com>
2023-06-10 16:36:54 +0530

    Add files via upload

---
+++ Email-Responder/EmailScheduler/MailScheduler.py
@@ -0,0 +1,16 @@
+#  Process Log with Periodic Mail Sending
+
+import os
+import time
+import psutil
+import smtplib
+import schedule
+from sys import *
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+def is_connected():
+    try:
+        urllib2.urlopen('http://216')
